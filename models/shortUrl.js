a31e0ecc5ca24c5ae312dc0bed7aa50c336f44dd
pridhvi02@gmail.com <pkm@6286>
2023-05-15 12:55:43 +0530

    first project

---
+++ models/shortUrl.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+const shortId = require("shortid");
+
+const shortUrlSchema = new mongoose.Schema({
+  full: {
+    type: String,
+  },
+  short: {
+    type: String,
+    default: shortId.generate,
+  },
+  clicks: {
+    type: Number,
+    default: 0,
+  },
+});
+
+module.exports = mongoose.model("ShortUrl", shortUrlSchema);
